1991203b6a39b453db57f945bdf99824cde6a901
yashwanth0912 <yash.cloudtech@gmail.com>
2025-08-21 13:17:25 +1000

    Demo

---
+++ 01_python_basics/demo.py
@@ -0,0 +1,14 @@
+#comments ignored by interpreter
+a=10
+b=20
+print(a+b)
+print('hello world') # SyntaxError: invalid syntax
+print(9) # number - int
+print(9.5) # number - float
+a = 10
+print(a)
+greetings = ("good morning") # text - string
+print(greetings)
+print('hello world')
+
+
